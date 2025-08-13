831356eb73056049fd77ae5c1282fe0027bf4ed6
MasterNassim <master.nassim@gmail.com>
2025-08-13 18:39:56 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <CardContent class

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-xl bg-card text-card-foreground shadow-md transition-all hover:shadow-lg",
+      "rounded-xl bg-card text-card-foreground shadow-md transition-all hover:shadow-lg active:scale-[0.98]",
       className
     )}
     {...props}
