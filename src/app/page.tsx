d89eee4c878dce3ef7e3cf9b70e922e6a9c0538a
MasterNassim <master.nassim@gmail.com>
2025-08-13 17:10:31 +0000

    anule les derniere modif

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,12 +17,7 @@ export default function LoginPage() {
             className="h-full w-full object-cover transition-transform duration-500 ease-in-out hover:scale-110 scale-125"
           />
         </div>
-        <div 
-            className="flex flex-col justify-center p-8 relative"
-            style={{
-                backgroundImage: `linear-gradient(to left, hsla(220, 13%, 96%, 0.3), hsla(220, 13%, 96%, 1)), url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='40' height='40' viewBox='0 0 40 40'%3E%3Cg fill-rule='evenodd'%3E%3Cg fill='%233b82f6' fill-opacity='0.1'%3E%3Cpath d='M0 38.59l2.83-2.83 1.41 1.41L1.41 40H0v-1.41zM0 1.4l2.83 2.83 1.41-1.41L1.41 0H0v1.41zM38.59 40l-2.83-2.83 1.41-1.41L40 38.59V40h-1.41zM40 1.41l-2.83 2.83-1.41-1.41L38.59 0H40v1.41zM20 18.6l2.83-2.83 1.41 1.41L21.41 20l2.83 2.83-1.41 1.41L20 21.41l-2.83 2.83-1.41-1.41L18.59 20l-2.83-2.83 1.41-1.41L20 18.59z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`,
-            }}
-        >
+        <div className="flex flex-col justify-center p-8">
             <CardHeader className="text-center">
               <div className="flex justify-center items-center gap-2 mb-4">
                   <Logo />
